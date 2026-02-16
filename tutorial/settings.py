9aa1803c68f7354e85d7b16ef3d77aaa0fc41968
InESS <iness@InESSs-MacBook-Pro.local>
2026-02-16 16:55:14 +0530

    Updated allowed hosts IP

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s7q@zer5gd)sc4veqfv964(xhubmju!@4^r2or%-@-ena8!ny1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.27.203.255']
 
 
 # Application definition
